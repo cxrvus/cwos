5d878db718da57e490647e66479bdcec0816ec44
cxrvus <benito.m.rabe@gmail.com>
2025-05-13 13:48:22 +0200

    minor Controller progress

--- src/controller.rs
+++ src/controller.rs
@@ -1,1 +1,22 @@
-pub struct Controller {}
+use crate::database::Database;
+
+#[derive(Default)]
+pub struct Controller {
+	database: Database,
+	stack: Vec<Routine>,
+}
+
+impl Controller {
+	pub fn new(db: Database) -> Self {
+		Self {
+			database: db,
+			..Default::default()
+		}
+	}
+
+	pub fn tick(&self) {
+		todo!()
+	}
+}
+
+pub struct Routine {}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
+use controller::Controller;
+use database::Database;
+
 mod char_converter;
 mod controller;
 mod database;
 
-fn main() {}
+fn main() {
+	let db = Database::load();
+	let ctl = Controller::new(db);
+}
