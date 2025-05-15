680f26a980e2ad967a5f1643a50006334b498cf3
cxrvus <benito.m.rabe@gmail.com>
2025-05-15 04:36:12 +0200

    Controller scaffolding progress

--- src/controller.rs
+++ src/controller.rs
@@ -1,9 +1,12 @@
 use crate::{database::Database, symbol_converter::Symbol};
 
+// todo: implement Peripherals as <T, U>
+// with fn tick(msg: T) -> U
+
 #[derive(Default)]
 pub struct Controller {
 	database: Database,
-	stack: Vec<Routine>,
+	stack: Vec<Box<dyn Routine>>,
 }
 
 impl Controller {
@@ -14,9 +17,23 @@ impl Controller {
 		}
 	}
 
-	pub fn tick(&self, cw: Option<Symbol>) -> Option<Symbol> {
-		todo!()
+	pub fn tick(&mut self, input: Option<Symbol>) -> Option<Symbol> {
+		// todo: add behavior for empty stack
+		let mut current_routine = self
+			.stack
+			.pop()
+			.expect("exited because no routine was active");
+
+		let result = current_routine.tick(self, input);
+
+		self.stack.push(current_routine);
+
+		result
 	}
+
+	// todo: create functions for pushing and pulling etc
 }
 
-pub struct Routine {}
+pub trait Routine {
+	fn tick(&mut self, cnt: &mut Controller, input: Option<Symbol>) -> Option<Symbol>;
+}
