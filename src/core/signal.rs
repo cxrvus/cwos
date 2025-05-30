3a84932f0fb5276ed9fb38fa57b99db959401942
cxrvus <benito.m.rabe@gmail.com>
2025-05-30 05:29:33 +0200

    major SignalController progress

--- src/core/signal.rs
+++ src/core/signal.rs
@@ -2,7 +2,7 @@ use super::{
 	context::CwContext,
 	database::{Config, SignalConfig},
 	routine::Routine,
-	symbol::{self, Signals, Symbol, SymbolConverter},
+	symbol::{SignalElements, Symbol, SymbolConverter},
 };
 
 #[derive(Default)]
@@ -12,10 +12,12 @@ pub enum Role {
 	User,
 }
 
-pub enum OutputState {
-	Remain, // remaining in the current state
-	On,     // e.g. a speaker starting to beep
-	Off,    // e.g. a speaker seizing to beep
+pub struct OutputState(pub bool);
+
+#[derive(Clone)]
+struct Signal {
+	is_on: bool,
+	duration: u32,
 }
 
 #[derive(Default)]
@@ -26,7 +28,7 @@ pub struct SignalController<T: Default, R: Routine<T>> {
 	routine: R,
 	active_role: Role,
 	last_input_state: bool,
-	buffer: Vec<u32>,
+	buffer: Vec<Signal>,
 	elapsed_ms: u32,
 }
 
@@ -65,103 +67,141 @@ impl<T: Default, R: Routine<T>> SignalController<T, R> {
 			(false, true) | (true, false) => {
 				// the if-clause prevents adding an Off-Signal duration to an empty duration buffer
 				if !self.buffer.is_empty() || self.last_input_state {
-					self.buffer.push(self.elapsed_ms);
+					self.buffer.push(Signal {
+						duration: self.elapsed_ms,
+						is_on: input_state,
+					});
 					self.elapsed_ms = 0;
 					self.last_input_state = input_state;
 				}
-
-				match input_state {
-					true => OutputState::On,
-					false => OutputState::Off,
-				}
 			}
-			(true, true) => OutputState::Remain, // todo: send Error Correction symbol if ms >= max
 			(false, false) => {
-				if self.elapsed_ms >= Self::MAX_MS {
+				// if the user is idle for long enough (but the buffer is not empty, meaning that signals have been transmitted)
+				// then send the user's buffer to the computer and return control
+				if !self.buffer.is_empty() && self.elapsed_ms >= Self::MAX_MS {
 					let conv = &self.ctx.symbol.clone();
-					let user_config = &self.user_config;
 
 					let input_signals = self.buffer.clone();
 					let input_symbols =
-						Self::durations_to_symbols(conv, user_config, input_signals);
+						Self::signals_to_symbols(conv, &self.user_config, input_signals);
 
 					let output_symbols = self.routine.tick(&mut self.ctx, input_symbols);
 					let output_signals =
-						Self::symbols_to_durations(conv, user_config, output_symbols);
+						Self::symbols_to_signals(conv, &self.comp_config, output_symbols);
 
 					self.buffer = output_signals;
 
 					self.active_role = Role::Comp;
 					self.elapsed_ms = 0;
 				}
-
-				OutputState::Remain
 			}
+			(true, true) => {}
 		}
+
+		OutputState(input_state)
 	}
 
 	fn comp_tick(&mut self) -> OutputState {
-		todo!()
+		if let Some(signal) = self.buffer.first() {
+			let output_state = signal.is_on;
+
+			if self.elapsed_ms >= signal.duration {
+				self.elapsed_ms = 0;
+
+				if !self.buffer.is_empty() {
+					self.buffer.remove(0);
+				}
+			}
+
+			OutputState(output_state)
+		} else {
+			self.elapsed_ms = 0;
+			self.active_role = Role::User;
+
+			OutputState(false)
+		}
 	}
 
-	fn durations_to_symbols(
+	fn signals_to_symbols(
 		conv: &SymbolConverter,
 		config: &SignalMsConfig,
-		durations: Vec<u32>,
+		signals: Vec<Signal>,
 	) -> Vec<Symbol> {
-		let mut signal_on = true;
-		let mut signals: Signals = Signals(vec![]);
+		let mut elements: SignalElements = SignalElements(vec![]);
 		let mut symbols: Vec<Symbol> = vec![];
 
-		// todo: handle odd duration counts
+		for signal in signals {
+			if signal.is_on {
+				// todo: send Error Correction symbol if ms >= max
+				// add a dah (true) or a dit (false)
+				elements.0.push(signal.duration >= config.dah_ms);
+			} else if signal.duration > config.break_ms {
+				// convert elements to a symbol if silence qualifies for a character break
+				symbols.push(conv.from_elements(&elements));
+				elements.0.clear();
 
-		for duration in durations {
-			if signal_on {
-				signals.0.push(duration >= config.dah_ms); // we get a dah (true) or a dit (false)
-				signal_on = false;
-			} else {
-				if duration > config.break_ms {
-					symbols.push(conv.from_signals(&signals));
-					signals.0.clear();
-
-					if duration > config.space_ms {
-						symbols.push(Symbol::Space);
-					}
+				// add a space
+				if signal.duration > config.space_ms {
+					symbols.push(Symbol::Space);
 				}
-				signal_on = true;
 			}
 		}
 
 		symbols
 	}
 
-	fn symbols_to_durations(
+	fn symbols_to_signals(
 		conv: &SymbolConverter,
 		config: &SignalMsConfig,
 		symbols: Vec<Symbol>,
-	) -> Vec<u32> {
-		let mut signal_on = true;
-		let mut durations: Vec<u32> = vec![];
-		let mut signals: Signals = Signals(vec![]);
+	) -> Vec<Signal> {
+		let mut signals: Vec<Signal> = vec![];
 
 		for symbol in symbols {
 			if let Symbol::Space = symbol {
-				durations.push(config.space_ms);
+				// remove the last silent signal element
+				if let Some(last) = signals.last() {
+					if !last.is_on {
+						signals.pop();
+					}
+				}
+
+				// add a space
+				signals.push(Signal {
+					is_on: false,
+					duration: config.space_ms,
+				});
 			} else {
-				let signals = conv.to_signals(&symbol);
-				for signal in signals.0 {
-					durations.push(match signal {
-						true => config.dah_ms,
-						false => config.dit_ms,
+				let elements = conv.to_elements(&symbol);
+				for signal in elements.0 {
+					// push either a dit or a dah
+					signals.push(Signal {
+						is_on: true,
+						duration: match signal {
+							true => config.dah_ms,
+							false => config.dit_ms,
+						},
 					});
 
-					durations.push(config.dit_ms); // the duration of silence after each signal is one dit
+					// element break - the duration of silence after each signal element is one dit
+					signals.push(Signal {
+						is_on: false,
+						duration: config.dit_ms,
+					});
 				}
-				durations.push(config.break_ms);
+
+				// remove the last element break to add a symbol break
+				signals.pop();
+
+				// add a break
+				signals.push(Signal {
+					is_on: false,
+					duration: config.break_ms,
+				});
 			}
 		}
 
-		durations
+		signals
 	}
 }
 
