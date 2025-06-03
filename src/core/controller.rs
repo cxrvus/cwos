652afdbc8cf948b76a129732f23525c032d0c0d9
cxrvus <benito.m.rabe@gmail.com>
2025-06-03 03:24:50 +0200

    misc Controller

--- src/core/controller.rs
+++ src/core/controller.rs
@@ -1,23 +1,30 @@
 use super::{context::CwContext, symbol::SymbolString};
 
 pub trait CwController<T>: Default {
-	fn tick(&mut self, ctx: &mut CwContext<T>, buffer: SymbolString) -> SymbolString;
+	fn tick(&mut self, ctx: &mut CwContext<T>, input: SymbolString) -> SymbolString;
 }
 
 #[derive(Default)]
 pub struct Echo;
 
 impl<T> CwController<T> for Echo {
-	fn tick(&mut self, _: &mut CwContext<T>, buffer: SymbolString) -> SymbolString {
-		buffer
+	fn tick(&mut self, _: &mut CwContext<T>, input: SymbolString) -> SymbolString {
+		input
 	}
 }
 
 #[derive(Default)]
-pub struct TestController;
+pub struct TestController {
+	is_done: bool,
+}
 
 impl<T> CwController<T> for TestController {
 	fn tick(&mut self, _: &mut CwContext<T>, _: SymbolString) -> SymbolString {
-		SymbolString::try_from("TEST".to_string()).unwrap()
+		if !self.is_done {
+			SymbolString::try_from("TEST".to_string()).unwrap()
+		} else {
+			self.is_done = true;
+			Default::default()
+		}
 	}
 }
