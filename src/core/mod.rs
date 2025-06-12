f9945fc1a1dd9c365bd09b58ba0918b3db165d28
cxrvus <benito.m.rabe@gmail.com>
2025-06-12 11:26:36 +0200

    rename signal_controller to signal

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -3,7 +3,7 @@ pub mod config;
 pub mod context;
 pub mod controller;
 pub mod element;
-pub mod signal_controller;
+pub mod signal;
 pub mod symbol;
 
 pub mod prelude {
@@ -12,6 +12,6 @@ pub mod prelude {
 	pub use super::context::*;
 	pub use super::controller::*;
 	pub use super::element::*;
-	pub use super::signal_controller::*;
+	pub use super::signal::*;
 	pub use super::symbol::*;
 }
